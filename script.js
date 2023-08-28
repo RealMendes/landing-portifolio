83ff0ed7c3b62bdcbdfbe909d36c3e0ab00b2278
Letícia Gabriela <70960662+RealMendes@users.noreply.github.com>
2023-08-28 19:49:04 -0300

    Implementação de contato, envio de email via API

--- script.js
+++ script.js
@@ -14,3 +14,40 @@ function updateSliderPosition() {
 }
 
 setInterval(nextSlide, 5000); // Auto slide every 5 seconds
+
+const btn = document.querySelector('button');
+const modal = document.querySelector('dialog');
+const closeBtn = document.querySelector('.close');
+const confirmationMessage = document.getElementById('confirmation-message');
+
+btn.onclick = function() {
+    modal.showModal();
+    confirmationMessage.style.display = "none"; // Oculta a mensagem ao abrir o modal
+}
+
+closeBtn.onclick = function() {
+    modal.close();
+    confirmationMessage.style.display = "none"; // Oculta a mensagem ao fechar o modal
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch("https://formsubmit.co/ajax/leehhoor@gmail.com", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+        },
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.success === "true") {
+            confirmationMessage.style.display = "block";
+        }
+    })
+    .catch(error => console.error(error));
+});
